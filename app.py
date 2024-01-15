a4a1ef55dfe06da15e59e2a2a047595b3f45e853
Feiyiaria <142081266+sunyiiihang@users.noreply.github.com>
2024-01-15 16:49:55 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, request, render_template
+import google.generativeai as palm
+palm.configure(api_key="AIzaSyCQpplzHGZdNIJ0d_xUUaSYdZGXiPUFNUI")
+model = {"model":"models/chat-bison-001"}
+app = Flask(__name__)
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        q = request.form.get("q")
+        print(q)
+        r = palm.chat(
+            **model,
+            messages = q
+        )
+        return(render_template("index.html",result=r.last))
+    else:
+        return(render_template("index.html", result="waiting for question............."))
+if __name__ == "__main__":
+    app.run(port=1234)
